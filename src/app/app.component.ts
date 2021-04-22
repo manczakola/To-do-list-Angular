bc7073ec59b316977402ab82815d64dc409c5414
Ola Mańczak <manczakola@gmail.com>
2021-04-22 21:54:29 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,86 @@
+import { Component } from '@angular/core';
+
+import { Task } from './tasks';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  taskName = 'Sugerowane zadanie codzienne: odkurzanie';
+  taskDate = '';
+
+  editMode: boolean = false;
+  config: { [key: string]: string } = null;
+  tasks: Task[] = [{
+    name: 'Siłownia',
+    deadline: '2020-01-02',
+    done: true,
+  },
+  {
+    name: 'Nauka Angulara',
+    deadline: '2020-01-03',
+    done: false,
+  },
+  {
+    name: 'Sprzątanie kuwety',
+    deadline: '2020-01-04',
+    done: false,
+  },]
+ 
+
+  constructor() {
+
+    setTimeout(() => {
+      this.config = {
+        title: "Lista zadań",
+        footer: "© Lista zadań zbudowana w Angularze",
+        date: new Date().toDateString()
+      }
+    }, 500);
+    this.sortTask();
+  }
+
+  clearTasks() {
+    this.tasks = [];
+    this.sortTask()
+  }
+
+
+  
+  createTask() {
+    const task: Task = {
+      name: this.taskName,
+      deadline: this.taskDate,
+      done: false,
+  }
+
+    this.tasks.push(task)
+    this.taskName = '';
+    this.taskDate = '';
+    this.sortTask()
+  }
+
+  switchEditMode() {
+    this.editMode = !this.editMode;
+  }
+
+
+  markTaskAsDone(task: Task) {
+    task.done = true;
+    this.sortTask()
+  }
+
+  deleteTask(task: Task) {
+    this.tasks = this.tasks.filter(t => t !== task);
+    this.sortTask();
+  }
+
+
+  private sortTask() {
+    this.tasks = this.tasks.sort((a: Task, b: Task) => 
+      a.done === b.done ? 0 : a.done ? 1 : -1
+  )
+  }
+}
